e673d5d986df2a7b0c6bc46f0996e2efe5009cb0
joao.modesto <galvaopp@gmail.com>
2024-04-21 19:27:16 -0300

    feat: finish pages

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
@@ -6,7 +7,12 @@ import examinationRoute from './routes/exanmination.routes';
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
 app.use('/examination', examinationRoute);
 
 
